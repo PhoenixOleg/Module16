a7be4fd43443352a0b71ec6057752d88b29aac1c
PhoenixOleg <metall_2000@mail.ru>
2023-11-25 23:03:17 +0300

    Добавлен тест для метода Subtraction класса Calculator

--- Module16.Tests/CalculatorTests.cs
+++ Module16.Tests/CalculatorTests.cs
@@ -11,5 +11,12 @@ namespace Module16.Tests
             Calculator calculator = new();
             Assert.AreEqual(7, calculator.Additional(2, 5));
         }
+
+        [Test]
+        public void Subtraction__MustReturnCorrectValue()
+        {  
+            Calculator calculator = new();
+            Assert.True (calculator.Subtraction(2, 5) == -3);
+        }
     }
 }
